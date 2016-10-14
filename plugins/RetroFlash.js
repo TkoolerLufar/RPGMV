b3a5248e97ec81d57ca3c85319aeed805457a2c0
TkoolerLufar <@>
2016-10-15 04:28:09 +0900

    パカパカ更新
    * 画面フラッシュの加乗算合成をボツに
    * オプションに設定項目を追加
    * プラグインコマンドでのモード変更をセーブするように
    * 画面が光る時に対象物が光って見えないバグを修正

--- plugins/RetroFlash.js
+++ plugins/RetroFlash.js
@@ -2,27 +2,71 @@
  * @plugindesc
  * Make flashes in animations rapid blinks.
  * 
+ * @param OptionLabel
+ * @default Another flash in battle
+ * @desc The label of configuration in the option list.
+ * If it's set empty, it doesn't appear in the option list.
+ * 
+ * @param OptionDefault
+ * @default 1
+ * @desc Default mode. Set 0 to disable in default.
+ * 
  * @help
- * You can disable this plugin on the game to run the script:
- *   Lufar.RetroFlash.enable = false;
- *//* For know about rapid blinks, google "Pokémon Shock."
+ * * Plugin Commands
+ * 
+ * RetroFlash enable
+ * : Enable this plugin.
+ *   It overrides the setting in the option.
+ * 
+ * RetroFlash disable
+ * : Disable this plugin.
+ *   It overrides the setting in the option.
+ * 
+ * RetroFlash reset
+ * : Reset the setting.
+ *   Then the setting in the option was refered.
+ * 
+ * * Caution
+ * 
+ * For know about rapid blinks, google "Pokémon Shock."
  */
 /*:ja
  * @plugindesc
- * アニメーションの対象物フラッシュを昔懐かしパカパカにし、
- * 画面のフラッシュを色に応じて加算ないし乗算合成にします。
+ * 戦闘アニメーションのフラッシュを昔懐かしパカパカにします。
+ * 処理が軽くなりますが目に強い刺激を与える場合があります。
  * 
- * @author ルファー (Twitter @tkooler_lufar)
+ * @author ついったtkooler_lufar
+ * 
+ * @param OptionLabel
+ * @default 戦闘時フラッシュ軽量化
+ * @desc オプションでパカパカの ON/OFF をする項目の名前。
+ * 空にするとオプションで設定できなくなります。
+ * 
+ * @param OptionDefault
+ * @default 1
+ * @desc パカパカを既定で有効にするかどうか。
+ * 0 にすると既定で無効になります。
  * 
  * @help
- * Lufar.RetroFlash.enable = false; で無効化できるようにしてあります。
+ * * プラグインコマンド
  * 
- * アニメーション対象物のフラッシュは常にパカパカですが、
- * 画面のフラッシュは色が暗いときだけパカパカになります。
- * 色が明るい場合、フラッシュは加算合成になります(通常合成より処理が軽そうだから)。
+ * RetroFlash enable
+ * : パカパカを有効化します。
+ *   オプションの設定より優先されます。
  * 
- * パカパカが視聴者に及ぼしうる影響については「パカパカ アニメーション」または「ポケモンショック」でググってください。
- * 念のため Lufar.RetroFlash.enable = false; で無効化できるようにしてあります。
+ * RetroFlash disable
+ * : パカパカを無効化します。
+ *   オプションの設定より優先されます。
+ * 
+ * RetroFlash reset
+ * : パカパカの設定を元に戻します。
+ *   再び enable/disable するまでオプションの設定に従います。
+ * 
+ * 
+ * * 備考
+ * 
+ * パカパカが視聴者に及ぼしうる影響については
+ * 「パカパカ アニメーション」または「ポケモンショック」でググってください。
  */
 
 // namespace
@@ -30,11 +74,77 @@ Lufar = Lufar || {};
 Lufar['RetroFlash'] = Lufar['RetroFlash'] || {};
 
 (function() {
-    this.enable = true;
+    /**
+     * おぷしょん JSON に入れるフラグの識別子
+     */
+    this.CONFIG_ID = 'Lufar_RetroFlash_enable';
 
+    /**
+     * パカパカが有効かどうか
+     * プラグインコマンドの設定が生きてればそれを参照し、
+     * 層でなければコンフィグを参照します。
+     */
+    Object.defineProperty(this, 'enabled', {
+        'get': function() {
+            var systemVlaue = this.systemEnabled;
+            if (systemVlaue) {
+                return systemVlaue > 0;
+            }
+            return this.configEnabled;
+        }
+    });
+
+    /**
+     * コンフィグの設定。保存されてなければデフォルトを参照します。
+     */
+    Object.defineProperty(this, 'configEnabled', {
+        'get': function() {
+            if (ConfigManager) {
+                return ConfigManager[this.CONFIG_ID];
+            }
+            return null;
+        },
+        'set': function(value) {
+            if(!ConfigManager) {
+                return !!value;
+            }
+            return ConfigManager[this.CONFIG_ID] == !!value;
+        }
+    });
+
+    /**
+     * プラグインコマンドの設定が有効かどうか
+     * 正の数⇒有効
+     * 負の数⇒無効
+     * 0⇒未設定
+     */
+    Object.defineProperty(this, 'systemEnabled', {
+        'get': function() {
+            if (
+                $gameSystem &&
+                $gameSystem['Lufar_RetroFlash']
+            ) {
+                return $gameSystem['Lufar_RetroFlash']['enabled'];
+            }
+            return 0;
+        },
+        'set': function(value) {
+            if (!$gameSystem) {
+                return !!value;
+            }
+            if(!$gameSystem['Lufar_RetroFlash']) {
+                $gameSystem['Lufar_RetroFlash'] = {};
+            }
+            return $gameSystem['Lufar_RetroFlash']['enabled'] = value;
+        }
+    });
+
+    /**
+     * 対象物フラッシュの更新
+     */
     var proto_updateFlash = Sprite_Animation.prototype.updateFlash;
     Sprite_Animation.prototype.updateFlash = function() {
-        if(!Lufar.RetroFlash.enable) {
+        if(!Lufar.RetroFlash.enabled) {
             return proto_updateFlash.call(this);
         }
         var d = this._flashDuration--;
@@ -47,9 +157,12 @@ Lufar['RetroFlash'] = Lufar['RetroFlash'] || {};
         }
     };
 
+    /**
+     * 画面フラッシュの更新
+     */
     var proto_updateScreenFlash = Sprite_Animation.prototype.updateScreenFlash;
     Sprite_Animation.prototype.updateScreenFlash = function() {
-        if (!Lufar.RetroFlash.enable) {
+        if (!Lufar.RetroFlash.enabled) {
             return proto_updateScreenFlash.call(this);
         }
         var d = this._screenFlashDuration--;
@@ -57,49 +170,98 @@ Lufar['RetroFlash'] = Lufar['RetroFlash'] || {};
             if (this._screenFlashSprite) {
                 this._screenFlashSprite.x = -this.absoluteX();
                 this._screenFlashSprite.y = -this.absoluteY();
-                if (this._screenFlashSprite.blendMode == PIXI.blendModes.NORMAL) {
-                    this._screenFlashSprite.visible = !!(d & 2);
+                if (this._screenFlashSprite.blendMode == Graphics.BLEND_NORMAL) {
+                    // 位相を対象物フラッシュとずらし、対象物フラッシュと丸かぶりしないように
+                    this._screenFlashSprite.visible = !!(d & 2) ^ !(d & 1);
                 } else {
                     this._screenFlashSprite.opacity *= (d - 1) / d;
                 }
             }
         }
+        else {
+            // フラッシュ終了時の後始末
+            this._screenFlashSprite.blendMode = Graphics.BLEND_NORMAL;
+        }
     };
 
+    /**
+     * 画面フラッシュの開始
+     */
     var proto_startScreenFlash = Sprite_Animation.prototype.startScreenFlash;
     Sprite_Animation.prototype.startScreenFlash = function(color, duration) {
         var ret = proto_startScreenFlash.call(this, color, duration);
-        if(!Lufar.RetroFlash.enable) {
-            return ret;
-        }
-        if (this._screenFlashSprite) {
-            if ((
-                Math.max(color[0], color[1], color[2]) + Math.min(color[0], color[1], color[2])
-            ) >= 255) {
-                this._screenFlashSprite.blendMode = PIXI.blendModes.ADD;
+        if (Lufar.RetroFlash.enabled) {
+            if (this._screenFlashSprite) {
+                if ((
+                    Math.max(color[0], color[1], color[2]) + Math.min(color[0], color[1], color[2])
+                ) >= 255) {
+                    this._screenFlashSprite.blendMode = Graphics.BLEND_ADD;
+                }
+                else {
+                    // this._screenFlashSprite.setColor(255,255,255);
+                    this._screenFlashSprite.blendMode = Graphics.BLEND_MULTIPLY;
+                }
+                // 画面フラッシュが加乗算合成できない問題が解決するまでパカパカ
+                this._screenFlashSprite.blendMode = Graphics.BLEND_NORMAL;
             }
-            else {
-                this._screenFlashSprite.blendMode = PIXI.blendModes.NORMAL;
-            }
         }
-    };
-    /*
-    Sprite_Animation.prototype.updateFlash = function() {
-        if (this._flashDuration > 0) {
-            var d = this._flashDuration--;
-            this._target.setBlendColor(this._flashDuration & 2 ? this._flashColor : [0, 0, 0, 0]);
-        }
+        return ret;
     };
 
-    Sprite_Animation.prototype.updateScreenFlash = function() {
-        if (this._screenFlashDuration > 0) {
-            var d = this._screenFlashDuration--;
-            if (this._screenFlashSprite) {
-                this._screenFlashSprite.x = -this.absoluteX();
-                this._screenFlashSprite.y = -this.absoluteY();
-                this._screenFlashSprite.visible = (this._screenFlashDuration > 0) && (this._screenFlashDuration & 2);
+    /**
+     * プラグインコマンド
+     */
+    var proto_pluginCommand = Game_Interpreter.prototype.pluginCommand;
+    Game_Interpreter.prototype.pluginCommand = function(command, args) {
+        if(command === 'RetroFlash') {
+            switch(args[0]) {
+            case 'enable':
+                Lufar.RetroFlash.systemEnabled = +1;
+                break;
+            case 'disable':
+                Lufar.RetroFlash.systemEnabled = -1;
+                break;
+            case 'reset':
+                Lufar.RetroFlash.systemEnabled = 0;
+                break;
             }
         }
-    };
-    //*/
+        return proto_pluginCommand.call(this, command, args);
+    }
+
+    /**
+     * コンフィグデータの作成
+     */
+    var proto_makeConfigData = ConfigManager.makeData;
+    ConfigManager.makeData = function() {
+        var config = proto_makeConfigData.call(this);
+        config[Lufar.RetroFlash.CONFIG_ID] = this[Lufar.RetroFlash.CONFIG_ID];
+        return config;
+    }
+
+    /**
+     * コンフィグデータの適用
+     */
+    var proto_applyConfigData = ConfigManager.applyData;
+    ConfigManager.applyData = function(config) {
+        proto_applyConfigData.call(this, config);
+        if (config[Lufar.RetroFlash.CONFIG_ID] !== undefined) {
+            this[Lufar.RetroFlash.CONFIG_ID] = this.readFlag(config, Lufar.RetroFlash.CONFIG_ID);
+        }
+        else {
+            this[Lufar.RetroFlash.CONFIG_ID] = Lufar.RetroFlash.defaultEnabled;
+        }
+    }
+
+    /**
+     * コンフィグ項目の作成
+     */
+    var proto_makeOptionCommandList = Window_Options.prototype.makeCommandList;
+    Window_Options.prototype.makeCommandList = function() {
+        proto_makeOptionCommandList.call(this);
+        var enableLabel = PluginManager.parameters('RetroFlash')['OptionLabel'];
+        if (enableLabel != '') {
+            this.addCommand(enableLabel, Lufar.RetroFlash.CONFIG_ID);
+        }
+    }
 }).call(Lufar['RetroFlash']);
